4e8c4778e6d61893e568a5d18f7b4384a61353ff
mayiran20 <69775714+mayiran20@users.noreply.github.com>
2020-09-14 09:27:39 +0800

    Create heart.py

---
+++ python code/heart.py
@@ -0,0 +1,11 @@
+this code is supposed to display small hearts and big hearts to show beating of the heart
+
+from microbit import *
+while True:
+  display.show(HEART_SMALL)
+  sleep(1000)
+  display.show(HEART)
+  sleep(1000)
+
+but my code does not work, can you help me ?
+  
